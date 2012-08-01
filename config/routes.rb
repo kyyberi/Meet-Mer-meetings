acdc6b7edefa41a1c69f6f19721e783ed43cb5ed
Jarkko (kyyberi) Moilanen <jarkko.moilanen@want3d.fi>
2012-08-01 22:22:34 +0300

    Added correct route to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 # CURRENT FILE :: config/routes.rb
+#Rails3BootstrapDeviseCancan::Application.routes.draw do
 Rails.application.routes.draw do
-  get "meetings" => "meetings/meeting#index" , :as => :meetings
+  #get "meetings" => "meetings/meeting#index" , :as => :meetings
+  resources :meetings
 end
